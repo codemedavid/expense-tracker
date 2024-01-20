330382fe9ea2d6fd063993483d629d4660181028
Jeff030197 <edwardparias3@gmail.com>
2024-01-20 22:52:00 +0800

    added spacebar

---
+++ app/Http/Controllers/IncomeController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Income;
+use Illuminate\Http\Request;
+
+class IncomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        {
+            $incomes = Income::all();
+            return view('app', compact('incomes'));
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('income.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'amount' => 'required|numeric',
+            'source' => 'required',
+        ]);
+
+        Income::create($request->all());
+
+        return redirect()->route('income.index');
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Income $income)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Income $income)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Income $income)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Income $income)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IncomeController;
 use Inertia\Inertia;
 
 /*
@@ -39,5 +40,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__ . '/auth.php';
